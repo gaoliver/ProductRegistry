99e1ff69e2db8177836c5cee464948ea96be7057
Gabriel Oliveira Costa Ramos <gabriel.oramos@b2wdigital.com>
2021-10-27 15:33:32 -0300

    feat(Profile): Initial structure

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,15 +1,63 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import LimitHeader from '../components/LimitHeader';
+import MainBox from '../components/MainBox';
 import MainContainer from '../components/MainContainer';
 import MainContent from '../components/MainContent';
+import Colors from '../constants/Colors';
+import { ApplicationReducer } from '../redux';
+import { UserModel } from '../utils/types';
 
 const ProfileScreen = () => {
-  const styles = StyleSheet.create({});
+  const user = useSelector(
+    (state: ApplicationReducer) => state.userReducer.user.user
+  );
+
+  const styles = StyleSheet.create({
+    boxContainer: {
+      alignItems: 'flex-start',
+      minHeight: 0,
+      width: '100%'
+    },
+    infoArea: {
+      width: '100%'
+    },
+    infoLabel: {
+      color: Colors.light.text,
+      marginBottom: 5
+    },
+    textDataContainer: {
+      width: '100%',
+      paddingVertical: 5,
+      paddingHorizontal: 10,
+      borderRadius: 5,
+      backgroundColor: Colors.light.background
+    },
+    textData: {
+      color: Colors.light.text
+    }
+  });
 
   return (
     <MainContainer>
+      <LimitHeader title={user.name} />
       <MainContent>
-        <Text>Olá</Text>
+        <MainBox boxStyle={styles.boxContainer}>
+          <View style={styles.infoArea}>
+            <Text style={styles.infoLabel}>Nome:</Text>
+            <View style={styles.textDataContainer}>
+              <Text style={styles.textData}>{user.name}</Text>
+            </View>
+          </View>
+          <View style={[styles.infoArea, { marginTop: 30 }]}>
+            <Text style={styles.infoLabel}>E-mail:</Text>
+            <View style={styles.textDataContainer}>
+              <Text style={styles.textData}>{user.email}</Text>
+            </View>
+          </View>
+        </MainBox>
       </MainContent>
     </MainContainer>
   );
